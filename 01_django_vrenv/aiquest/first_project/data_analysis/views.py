10adb7612052c48e6765d8560305b151acd9858e
sagorluc <mdsagorluc@gmail.com>
2023-07-23 23:49:38 +0600

    update

--- 01_django_vrenv/aiquest/first_project/data_analysis/views.py
+++ 01_django_vrenv/aiquest/first_project/data_analysis/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse as hr
 def data_ana(request):
     #return hr('<h1>this is data_analysis app</h1>')
     
-    return render(request,'data_analysis.html')
+    return render(request,'data_analysis/data_analysis.html')
--- 01_django_vrenv/aiquest/first_project/deep_learning/views.py
+++ 01_django_vrenv/aiquest/first_project/deep_learning/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse as hr
 # Create your views here.
 def dp_learning(request):
     # return hr('<h1>this is deep_learning apps</h1>')
-    return render(request,'deep_learning.html')
+    return render(request,'deep_learning/deep_learning.html')
